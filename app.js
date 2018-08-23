59e660dff4f434bdd0437143fa55bc757e24fcb6
atsushi <tamesitegatten2@nifty.com>
2018-08-23 12:07:01 +0900

    bmiprogram

---
+++ app.js
@@ -0,0 +1,13 @@
+var r=new Ractive({
+    el:'#container',
+    template:'#template'
+})
+r.on({
+    
+    'clicked':()=>{
+    var high=r.get('high')
+    var weight=r.get('weight')
+    var bmi=weight/(high*high)
+    r.set('BMI',bmi)
+} 
+})
